0ed3c025260da9a154753a05986da4d3b01cd04a
Unknown <unknown@example.com>
2017-07-16 01:03:17 +0800

    nodemon, fs

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    var users = [
+        { name: 'Debbie' },
+        { name: 'Jirffey' }
+    ]
+
+    res.send(users);
 });
 
 module.exports = router;
