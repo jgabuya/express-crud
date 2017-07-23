3f3ee4fa12b208759febf230ce5e00043b1aaf46
Jirffey Gabuya <boyhimas@gmail.com>
2017-07-23 23:48:50 +0800

    routes, views

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET contacts listing. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
     res.render('index', { title: 'All Contacts' });
+});
+
+router.get('/create', (req, res, next) => {
+    res.render('create', { title: 'Create New Contact' });
+});
+
+router.post('/store', (req, res, next) => {
+    res.send(req.params);
+});
+
+router.get('/edit/:contactId', (req, res, next) => {
+    res.send(req.params);
+});
+
+router.post('/update/:contactId', (req, res, params) => {
+    res.send(req.params);
 });
 
 module.exports = router;
